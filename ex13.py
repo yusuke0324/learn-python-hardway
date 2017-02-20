cdf84cbe4185095e60da88e7db25524389cc7ed7
yusuke0324 <take.yusuke@gmail.com>
2017-02-21 00:10:19 +0900

    Complete ~ex15

---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+# var1, var2, var3 = [1, 2, 3] gonna be
+# var1 = 1, var2 = 2, var3 = 3
+script, first, second, third = argv
+print argv
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is;", third

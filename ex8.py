a933c0dbc5831282f4f66ff0d43cbb6769d8dc92
yusuke0324 <take.yusuke@gmail.com>
2017-02-20 21:00:21 +0900

    Complete ex8

---
+++ ex8.py
@@ -0,0 +1,13 @@
+# see how %s and %r are different
+formatter = "%s %r %r %r"
+
+print formatter % (1,2,3,4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+    "I had this thing.",
+    "That you could type up right.",
+    "But if didn't sing.",
+    "So I said goodnight."
+  )
